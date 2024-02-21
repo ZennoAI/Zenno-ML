7cb7bd1e1e9b4d787004e33173255f8a19586849
Diego Sabajo <diegofranco711@gmail.com>
2024-02-21 19:33:02 -0300

    disabled whylogs

--- service.py
+++ service.py
@@ -7,8 +7,8 @@ from src.data_processing import DataProcessor
 from src.vector_store import VectorStoreManager
 from src.data import data
 from src.array_json_io import ArrayJSONIODescriptor 
-from src.llm_monitoring.whylogs import log_prompt_response, is_not_toxic
-from src.lllm_security.security import is_not_prompt_injection
+# from src.llm_monitoring.whylogs import log_prompt_response, is_not_toxic
+# from src.lllm_security.security import is_not_prompt_injection
 
 load_dotenv()
 
